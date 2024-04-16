9ebab387d99029ca15005bed6cd1d484e8162c6a
kevinbermudes <kevinber783@gmail.com>
2024-04-16 17:17:46 +0200

    Se añade para ver los mensajes de error

---
+++ src/main/kotlin/com/example/torneo_kotlin/Rest/torneo/controllers/GlobalExceptionHandler.kt
@@ -0,0 +1,27 @@
+package com.example.torneo_kotlin.Rest.torneo.controllers
+
+import org.springframework.http.HttpStatus
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, Any> {
+        val errors = mutableMapOf<String, Any>()
+        ex.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.getDefaultMessage()
+            errors[fieldName] = mapOf(
+                "rejectedValue" to error.rejectedValue,
+                "errorMessage" to errorMessage
+            )
+        }
+        return errors
+    }
+}
